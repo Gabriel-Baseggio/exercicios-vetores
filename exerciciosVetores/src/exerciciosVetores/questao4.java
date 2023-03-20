a27766e1b8e913f0fdf86ab6c7357b1297c14067
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2023-03-20 20:28:46 -0300

    teste

--- exerciciosVetores/src/exerciciosVetores/questao4.java
+++ exerciciosVetores/src/exerciciosVetores/questao4.java
@@ -6,23 +6,25 @@ public class questao4 {
 
 	public static void main(String[] args) {
 		
+		//teste
+		
 		double somaPesos = 0, nota;
 		double[] mediasPonderadas, pesosProvas, somaNotasProva;
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Digite o número de alunos: ");
+		System.out.print("Digite o nï¿½mero de alunos: ");
 		mediasPonderadas = new double[sc.nextInt()];
 		
 		System.out.println();
 		
-		System.out.print("Digite o número de provas: ");
+		System.out.print("Digite o nï¿½mero de provas: ");
 		pesosProvas = new double[sc.nextInt()];
 		somaNotasProva = new double[pesosProvas.length];
 		
 		System.out.println();
 		
 		for (int i = 0; i < pesosProvas.length; i++) {
-			System.out.print("Digite o peso da " + (i + 1) + "ª prova: ");
+			System.out.print("Digite o peso da " + (i + 1) + "ï¿½ prova: ");
 			pesosProvas[i] = sc.nextDouble();
 			somaPesos += pesosProvas[i];
 		}
@@ -31,7 +33,7 @@ public class questao4 {
 		
 		for (int i = 0; i < mediasPonderadas.length; i++) {
 			for (int j = 0; j < pesosProvas.length; j++) {
-				System.out.print("Digite a nota do " + (i + 1) + "º aluno na " + (j + 1) + "ª prova: ");
+				System.out.print("Digite a nota do " + (i + 1) + "ï¿½ aluno na " + (j + 1) + "ï¿½ prova: ");
 				nota = sc.nextDouble();
 				somaNotasProva[j] += nota;
 				mediasPonderadas[i] += nota*pesosProvas[j];
@@ -41,16 +43,16 @@ public class questao4 {
 		
 		System.out.println();
 		
-		System.out.println("Médias ponderadas por aluno");
+		System.out.println("Mï¿½dias ponderadas por aluno");
 		for (int i = 0; i < mediasPonderadas.length; i++) {
-			System.out.println("Média ponderada do " + (i + 1) + "º aluno: " + mediasPonderadas[i]);
+			System.out.println("Mï¿½dia ponderada do " + (i + 1) + "ï¿½ aluno: " + mediasPonderadas[i]);
 		}
 		
 		System.out.println();
 		
-		System.out.println("\nMédias aritméticas por prova: ");
+		System.out.println("\nMï¿½dias aritmï¿½ticas por prova: ");
 		for (int i = 0; i < pesosProvas.length; i++) {
-			System.out.println("Média aritmética da " + (i + 1) + "ª prova: " + (somaNotasProva[i])/mediasPonderadas.length);
+			System.out.println("Mï¿½dia aritmï¿½tica da " + (i + 1) + "ï¿½ prova: " + (somaNotasProva[i])/mediasPonderadas.length);
 		}
 		
 		sc.close();
