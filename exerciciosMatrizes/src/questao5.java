36e87ac13457090711c8bee1e05fb889e1a1a472
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2023-04-03 15:29:34 -0300

    exerciciosMatrizes - 03/04

---
+++ exerciciosMatrizes/src/questao5.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class questao5 {
+
+	public static void main(String[] args) {
+		
+		int[][] matriz = new int[4][4];
+		Scanner sc = new Scanner(System.in);
+		
+		for (int ln = 0; ln < matriz.length; ln++) {
+			for (int col = 0; col < matriz[0].length; col++) {
+				System.out.print("Digite o " + (col + 1) + "º valor da " + (ln + 1) + "ª linha da matriz: ");
+				matriz[ln][col] = sc.nextInt();
+			}
+		}
+		
+		for (int ln = 0; ln < matriz.length; ln++) {
+			for (int col = 0; col < matriz[0].length; col++) {
+				if (ln == col) {
+					System.out.print(matriz[ln][col] + " ");
+				}
+			}
+		}
+		
+		System.out.println();
+		
+		for (int ln = 0; ln < matriz.length; ln++) {
+			for (int col = 0; col < matriz[0].length; col++) {
+				if (ln != col) {
+					System.out.print(matriz[ln][col] + " ");
+				}
+			}
+		}
+		
+		
+		sc.close();
+
+	}
+
+}
