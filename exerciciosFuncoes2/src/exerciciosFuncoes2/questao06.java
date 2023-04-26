e362d4c8172d862f5a2d4686d9ed60f3fde5de50
Gabriel-Baseggio <100075703+Gabriel-Baseggio@users.noreply.github.com>
2023-04-26 14:05:54 -0300

    Update questao06.java

--- exerciciosFuncoes2/src/exerciciosFuncoes2/questao06.java
+++ exerciciosFuncoes2/src/exerciciosFuncoes2/questao06.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 public class questao06 {
 
-	static char[] alfabeto = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-			's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-
 	public static void main(String[] args) {
 
 		char caractere;
@@ -15,19 +12,17 @@ public class questao06 {
 		System.out.print("Digite um caractere: ");
 		caractere = sc.next().charAt(0);
 
-		System.out.println("É caractere minúsculo: " + checarCaractere(caractere));
+		System.out.println("Ã‰ alfabÃ©tico minÃºsculo: " + checarCaractere(caractere));
 
 		sc.close();
 	}
 
 	static boolean checarCaractere(char caractere) {
 
-		for (int i = 0; i < alfabeto.length; i++) {
-			if (caractere == alfabeto[i]) {
-				return true;
-			}
+		if (Character.toString(caractere).matches("[a-z?]")) {
+			return true;
 		}
-
+		
 		return false;
 
 	}
