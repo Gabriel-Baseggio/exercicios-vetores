bafcac75764e840552b1d975222596d57a570696
Gabriel-Baseggio <100075703+Gabriel-Baseggio@users.noreply.github.com>
2023-04-26 14:04:12 -0300

    Update questao04.java

--- exerciciosFuncoes2/src/exerciciosFuncoes2/questao04.java
+++ exerciciosFuncoes2/src/exerciciosFuncoes2/questao04.java
@@ -4,10 +4,6 @@ import java.util.Scanner;
 
 public class questao04 {
 
-	static char[] alfabeto = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-			'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
 	public static void main(String[] args) {
 
 		char caractere;
@@ -17,17 +13,15 @@ public class questao04 {
 		caractere = sc.next().charAt(0);
 
 
-		System.out.println("� caractere: " + checarCaractere(caractere));
+		System.out.println("É alfabético: " + checarCaractere(caractere));
 
 		sc.close();
 	}
 
 	static boolean checarCaractere(char caractere) {
 
-		for (int i = 0; i < alfabeto.length; i++) {
-			if (caractere  == alfabeto[i]) {
-				return true;
-			}
+		if (Character.toString(caractere).matches("[A-Z?]") || Character.toString(caractere).matches("[a-z?]")) {
+			return true;
 		}
 		
 		return false;
