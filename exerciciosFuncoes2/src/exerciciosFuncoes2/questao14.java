72ab377fc7669d096a0bd169ce21dd3e80f70ba6
Gabriel-Baseggio <100075703+Gabriel-Baseggio@users.noreply.github.com>
2023-04-26 14:13:03 -0300

    Update questao14.java

--- exerciciosFuncoes2/src/exerciciosFuncoes2/questao14.java
+++ exerciciosFuncoes2/src/exerciciosFuncoes2/questao14.java
@@ -12,14 +12,14 @@ public class questao14 {
 		System.out.print("Digite uma cadeia de caracteres: ");
 		cadeia = sc.next();
 
-		System.out.println("É um palíndromo: " + checarPalindromo(cadeia));
+		System.out.println("Ã‰ um palÃ­ndromo: " + checarPalindromo(cadeia));
 
 		sc.close();
 	}
 
 	static boolean checarPalindromo(String cadeia) {
 
-		for (int i = 0; i < cadeia.length(); i++) {
+		for (int i = 0; i < (cadeia.length() / 2); i++) {
 			if (cadeia.charAt(i) != cadeia.charAt((cadeia.length() - 1) - i)) {
 				return false;
 			}
